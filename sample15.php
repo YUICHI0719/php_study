2af522bca413b311acd5bb3fe562af7865ba7af2
Yuichi Kanno <yuichi.kanno@nijibox.co.jp>
2020-05-12 16:12:21 +0900

    foreachの修正

--- sample15.php
+++ sample15.php
@@ -20,11 +20,11 @@
 <pre>
 <?php
   $xmlTree = simplexml_load_file('https://h2o-space.com/feed/');
-  foreach($xmlTree->channel->item as $item);
+  foreach($xmlTree->channel->item as $item):
 ?>
 ・<a href="<?php print($item->link); ?>"><?php print($item->title); ?></a>
 <?php
-  // endforeach;
+  endforeach;
 ?>
 </pre>
 </main>
